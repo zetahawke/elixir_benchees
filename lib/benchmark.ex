199d355f406138faec7f68cdf15bcd440fd68204
zetahawke <michel.szinavel@gmail.com>
2020-02-14 10:26:56 -0300

    Adding benchee default test schema

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -15,4 +15,34 @@ defmodule Benchmark do
   def hello do
     :world
   end
+
+  def some_fun_stuff do
+    Benchee.run(%{
+      "old_algorithm api"    => fn(list) -> 
+        url = "http://localhost:6000/api/prices"
+        headers = []
+
+        response = HTTPoison.get(url, headers, params: params)
+        Poison.decode(response.body)
+      end,
+      "new_algorithm api" => fn(list) -> 
+        url = "http://localhost:6000/api/quotations"
+        headers = []
+
+        response = HTTPoison.get(url, headers, params: params)
+        Poison.decode(response.body)
+      end
+    },
+      formatters: [
+        {Benchee.Formatters.HTML, file: "samples_output/my.html"},
+        Benchee.Formatters.Console
+      ],
+      time: 10,
+      warmup: 2,
+      inputs: %{
+        "Smaller List" => Enum.to_list(1..100),
+        "Bigger List"  => Enum.to_list(1..2_000),
+      }
+    )
+  end
 end
